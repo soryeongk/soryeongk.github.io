d8d1707f6ed27581868c4d080c511cfc1abc5cf0
soryeongk <soryeong.kim@autopediacar.com>
2023-01-24 16:41:57 +0900

    :tada: feat: 하단 네비게이션 추가

---
+++ frontend/components/BottomNavigation.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const BottomNavigation = () => {
+  const router = useRouter();
+
+  const navigationUrls = [
+    {
+      url: "",
+      text: "소개",
+    },
+    {
+      url: "comment",
+      text: "방명록",
+    },
+    {
+      url: "projects",
+      text: "프로젝트",
+    },
+  ];
+
+  return (
+    <nav className="sticky bottom-0 left-0 right-0 flex items-center justify-between px-3 py-2 bg-gray-100 border-t border-t-blue-dark">
+      {navigationUrls.map((navigation) => (
+        <Link key={navigation.url} href={`/${navigation.url}`} passHref>
+          <span
+            className={`${
+              router.pathname === `/${navigation.url}`
+                ? "font-bold"
+                : "text-gray-500"
+            }`}
+          >
+            {navigation.text}
+          </span>
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default BottomNavigation;
---
+++ frontend/pages/projects/index.page.tsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout";
+
+const ProjectsPage = () => {
+  return (
+    <Layout>
+      <div>프로젝트 페이지</div>
+    </Layout>
+  );
+};
+
+export default ProjectsPage;
