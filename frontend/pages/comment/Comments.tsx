0746b2ab7c28ec282e0da32658c368d9e54ef225
soryeongk <soryeong.kim@autopediacar.com>
2023-01-28 01:24:01 +0900

    :tada: feat: react suspense와와 error boundary 추가

--- frontend/pages/comment/Comments.tsx
+++ frontend/pages/comment/Comments.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import CloseIcon from "../../components/icons/CloseIcon";
 import {
   useReadComments,
@@ -5,24 +7,24 @@ import {
 } from "../../hooks/query/comment";
 
 const Comments = () => {
-  const { data, isLoading, isError } = useReadComments();
+  const { data } = useReadComments();
   const deleteCommentMutation = useDeleteCommentMutation();
 
   const clickDeleteComment = (commentId: string) => {
     deleteCommentMutation.mutate(commentId);
   };
 
-  if (isLoading) {
-    return <div>댓글 목록을 불러오는 중이에요!</div>;
-  }
+  // if (isLoading) {
+  //   return <div>댓글 목록을 불러오는 중이에요!</div>;
+  // }
 
-  if (!data || isError) {
-    return <div>댓글 목록을 불러오는데 실패했어요ㅠ</div>;
-  }
+  // if (!data || isError) {
+  //   return <div>댓글 목록을 불러오는데 실패했어요ㅠ</div>;
+  // }
 
   return (
     <ul className="flex flex-col-reverse gap-y-[1px] bg-transparent">
-      {data.comments.map((comment) => (
+      {data?.comments.map((comment) => (
         <li
           key={comment._id}
           className="relative flex flex-col gap-y-1 py-5 px-3 bg-gray-100"
--- frontend/pages/index.page.tsx
+++ frontend/pages/index.page.tsx
@@ -1,40 +1,32 @@
-import Image from "next/image";
 import React from "react";
-import { useQuery } from "react-query";
+import Loader from "react-spinners/BeatLoader";
 import { RecoilRoot } from "recoil";
 
-import { getMyGithubInfo } from "../axios/githubUser";
-import { QUERY_KEY } from "../axios/types";
+import ErrorBoundary from "../components/ErrorBoundary";
 import Layout from "../components/Layout";
+import { themeColors } from "../styles/theme";
 
-import Activities from "./index/Activities";
 import Navigation from "./index/Navigation";
 
 export default function Home() {
-  const { data } = useQuery(QUERY_KEY.GithubUser, getMyGithubInfo);
+  const ProfileSection = React.lazy(() => import("./index/ProfileSection"));
+  const Activities = React.lazy(() => import("./index/Activities"));
 
   return (
     <Layout>
       <div className="flex flex-col items-center gap-y-1">
-        <div className="flex flex-col items-center gap-y-4 py-3 bg-gray-100 w-full">
-          <div className="relative w-[300px] h-[300px] border border-gray-500 rounded-full overflow-hidden">
-            <Image
-              src={
-                data?.avatar_url ||
-                "https://avatars.githubusercontent.com/u/40630964?v=4"
-              }
-              alt="프로필 사진"
-              fill={true}
-            />
-          </div>
-          <div className="flex items-end">
-            <h1 className="text-3xl font-bold">김소령</h1>
-            {data?.bio && <h2 className="text-lg">: {data.bio}</h2>}
-          </div>
-        </div>
+        <ErrorBoundary>
+          <React.Suspense fallback={<Loader color={themeColors.blue.dark} />}>
+            <ProfileSection />
+          </React.Suspense>
+        </ErrorBoundary>
         <RecoilRoot>
           <Navigation />
-          <Activities />
+          <ErrorBoundary>
+            <React.Suspense fallback={<Loader color={themeColors.blue.dark} />}>
+              <Activities />
+            </React.Suspense>
+          </ErrorBoundary>
         </RecoilRoot>
       </div>
     </Layout>
