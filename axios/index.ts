fb14f02cc34c0b4738b23ad39cdfde6c95dd4cb6
soryeongk <soryeong.kim@autopediacar.com>
2023-04-22 17:05:23 +0900

    feat: api 주소 변경

--- axios/index.ts
+++ axios/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseInstance = axios.create({
-  baseURL: "https://54.180.156.29:4000",
+  baseURL: "http://api.soryeongk.com:4000",
   headers: {
     "Content-Type": "application/json",
   },
