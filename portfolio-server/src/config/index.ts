b684fe9c2b4b8c8407d91ee90d54c5948e8d3eba
soryeongk <soryeong.kim@autopediacar.com>
2023-04-22 17:13:30 +0900

    fix: 불필요한 서버 코드 제거

--- portfolio-server/src/config/index.ts
+++ portfolio-server/src/config/index.ts
@@ -1,8 +1,5 @@
 import dotenv from "dotenv";
 
-// Set the NODE_ENV to 'development' by default
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
 const envFound = dotenv.config();
 
 if (envFound.error) {
